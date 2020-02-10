f7ec6aab7d4a05e6f4796fc3725488a21bbc90b4
Shain Collins <Scolli03.sc@gmail.com>
2020-02-10 15:40:17 -0600

    Add project files.

---
+++ LearnAspNetCore/Startup.cs
@@ -0,0 +1,63 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace LearnAspNetCore
+{
+    public class Startup
+    {
+        private IConfiguration _config;
+
+        public Startup(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            //DefaultFilesOptions defaultFilesOptions = new DefaultFilesOptions();
+            //defaultFilesOptions.DefaultFileNames.Clear();
+            //defaultFilesOptions.DefaultFileNames.Add("foo.html");
+            //app.UseDefaultFiles(defaultFilesOptions);
+
+
+            FileServerOptions defaultFilesOptions = new FileServerOptions();
+            defaultFilesOptions.DefaultFilesOptions.DefaultFileNames.Clear();
+            defaultFilesOptions.DefaultFilesOptions.DefaultFileNames.Add("foo.html");
+
+            app.UseFileServer();
+            
+
+            app.Run(async (context) =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+
+            //app.UseRouting();
+
+            //app.UseEndpoints(endpoints =>
+            //{
+            //    endpoints.MapGet("/", async context =>
+            //    {
+            //        await context.Response.WriteAsync(_config["MyKey"]);
+            //    });
+            //});
+        }
+    }
+}

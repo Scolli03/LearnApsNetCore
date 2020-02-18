dd14f96b5e23688561d1d99ad38744cc5b8cf63c
Shain Collins <Scolli03.sc@gmail.com>
2020-02-18 16:22:35 -0600

    Attribute routing on Home Controller...Changes Deleted after this commit. REMIDER! THE CHANGES FROM THIS EP.33 ARE ONLY IN THIS COMMIT

--- LearnAspNetCore/Controllers/HomeController.cs
+++ LearnAspNetCore/Controllers/HomeController.cs
@@ -8,6 +8,8 @@ using System.Threading.Tasks;
 
 namespace LearnAspNetCore.Controllers
 {
+    //[Route("Home")]
+    [Route("[controller]/[action]")]
     public class HomeController:Controller
     {
         private readonly IEmployeeRepository _employeeRespository;
@@ -17,9 +19,12 @@ namespace LearnAspNetCore.Controllers
             _employeeRespository = employeeRepository;
         }
 
+        //other attribute routing options
+        //[Route("Home")]
+        //[Route("Home/Index")]
+        //[Route("[action]")]
         [Route("")]
-        [Route("Home")]
-        [Route("Home/Index")]
+        [Route("~/")]
         public ViewResult Index()
         {
             var model = _employeeRespository.GetAllEmployee();
@@ -27,12 +32,17 @@ namespace LearnAspNetCore.Controllers
             return View(model);
         }
 
-        public ViewResult Details(int id)
+        //other attribute routing options
+        //[Route("Home/Details/{id?}")]
+        //[Route("[action]")]
+
+        [Route("{id?}")]
+        public ViewResult Details(int? id)
         {
 
             HomeDetailsViewModel homeDetailsViewModel = new HomeDetailsViewModel()
             {
-                Employee = _employeeRespository.GetEmployee(id),
+                Employee = _employeeRespository.GetEmployee(id ?? 1),
                 PageTitle = "Employee Details"
             };
             
